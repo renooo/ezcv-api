73f2ef91ccfeab04e666617de8d73b7ea2ddac1c
Renaud <renaud.bougre@gmail.com>
2015-01-29 11:48:40 +0100

    create dans ExperienceResource

--- module/Api/src/Api/V1/Rest/Experience/ExperienceResource.php
+++ module/Api/src/Api/V1/Rest/Experience/ExperienceResource.php
@@ -5,6 +5,14 @@ use ZF\Apigility\Doctrine\Server\Resource\DoctrineResource;
 
 class ExperienceResource extends DoctrineResource
 {
+	function create($data)
+	{
+		if($this->getInputFilter())
+			$data = $this->getInputFilter()->getValues();
+		
+		return parent::create($data);
+	}
+
 	function update($id, $data)
 	{
 		if($this->getInputFilter())
